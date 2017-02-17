d012ead53f67efa0f2cb9e168ef9e55c724dd202
gizemny <gizemny@gmail.com>
2017-02-16 23:06:34 -0600

    made all these sexy new components - kicking ass

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -5,7 +5,7 @@ class StorePicker extends React.Component {
 		return ( 
 			<form className="store-selector"> 
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" />
+				<input type="text" required placeholder="Store Name"/>
 				<button type="submit">Visit Store →</button>
 			</form>
 		)
