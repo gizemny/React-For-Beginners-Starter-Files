c02795532b870b77a9200402dd76d94be2bfd5e6
gizemny <gizemny@gmail.com>
2017-02-19 21:29:44 -0600

    added events to store picker to generate storename and update URL via router

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,33 @@
 import React from 'react'; 
-
+import { getFunName } from '../helpers';
 class StorePicker extends React.Component { 
+	//this doesnt mean storepicker because we wrote own method
+	//need to build constructor! 
+	// constructor() { // need this when methods are used more than once
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// } // using it on submit is duplicitous 
+	goToStore(event) {
+		event.preventDefault(); // stopped form from submitting
+		// grab text from box - don't touch the DOM 
+		const storeId = this.storeInput.value;
+		// transition from / to / store/:storeId
+		this.context.router.transitionTo(`/store/${storeId}`);
+	} // render methods are bound to component 
 	render() {
 		return ( 
-			<form className="store-selector"> 
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}> 
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name"/>
+				<input type="text" required placeholder="Store Name" defaultValue={
+					getFunName()} ref={(input) => { this.storeInput = input }} />
 				<button type="submit">Visit Store →</button>
 			</form>
 		)
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker; 
