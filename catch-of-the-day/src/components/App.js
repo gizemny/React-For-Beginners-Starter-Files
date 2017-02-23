3053e2d51e4a7d5fe9e451f67d85dc95a1e017b6
gizemny <gizemny@gmail.com>
2017-02-22 20:38:12 -0600

    whew! learning about states. Used component, methods, constructor, and more 😅

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,23 @@ import Order from './Order';
 import Inventory from './Inventory'; 
 
 class App extends React.Component { 
+	constructor() { // get initial state w/ constructor
+		super();
+		this.addFish = this.addFish.bind(this);
+		this.state = {
+			fishes: {},
+			order: {}
+		}
+	} 
+	// new method:
+	addFish(fish) {
+		const fishes = {... this.state.fishes}
+		//add in our new fish
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+		//set state
+		this.setState({fishes: fishes})
+	}
 	render() {
 		return ( 
 			<div className="catch-of-the-day">
@@ -11,7 +28,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market"/>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</div>
 		)
 	}
